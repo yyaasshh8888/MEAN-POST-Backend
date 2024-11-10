084eb82b98ab9ce61798a9ff3213ff06340e837a
yyaasshh <yashjpawar5798@gmail.com>
2024-11-10 17:25:18 +0530

    File path changes

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -3,10 +3,14 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    const decodedToken = jwt.verify(
+      token,
+      process.env.JWT_SECRET || "SUPERSECRET"
+    );
     req.userData = decodedToken;
     next();
   } catch (error) {
+    console.log(error);
     return res.status(401).json({
       message: "You're not authenticated",
     });
